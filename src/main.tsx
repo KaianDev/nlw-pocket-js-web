eb4503026f9ce153f9c7dc4fcdffa1f610f33a97
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-09-12 23:47:41 -0300

    chore: setup tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./app"
 import "./index.css"
+import { Providers } from "./providers"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </StrictMode>
 )
